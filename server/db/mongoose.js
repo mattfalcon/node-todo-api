6ca047e7bfdb001eca57690edaa4acad7e4fbb3d
matt <mattvalladarez@gmail.com>
2017-07-16 15:25:37 -0500

    port

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 //connect to database 
 //which promise library we want to use built in promise library
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 module.exports = {mongoose};
